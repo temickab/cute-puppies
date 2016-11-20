3b5f8f8054c0c2e692420cae8623c654116befdb
temickab <mekapleio@yahoo.com>
2016-11-20 18:38:41 -0500

    will be updating this in the near future

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+$('.fade').slick({
+setting-name: setting-value
+});
+    
+    
+    
+$('.fade').slick({
+dots: true,
+infinite: true,
+speed: 500,
+fade: true,
+cssEase: 'linear'
+
+});
+
+
+    
+    
+    
+    
+});
